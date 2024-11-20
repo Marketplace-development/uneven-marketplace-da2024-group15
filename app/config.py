5f15178764446037125872334fe5322ec82c6c6e
tlanckriet <tibe.lanckriet@Ugent.be>
2024-11-20 10:53:35 +0100

    test verwijderen

--- app/config.py
+++ app/config.py
@@ -2,4 +2,3 @@ class Config:
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-print("test")
