251c07b28d80997006f496ee3b95ecc95a396f37
anseeuwlara <lara.anseeuw@ugent.be>
2024-11-28 13:29:23 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from sqlalchemy import TIMESTAMP, func
 from sqlalchemy.dialects.postgresql import NUMERIC
@@ -12,14 +11,6 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     registration_date = db.Column(TIMESTAMP(timezone=True), server_default=func.now())
     email = db.Column(db.String(120), unique=True, nullable=False)
-    notification = db.Column(db.Boolean, default=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.user_id}: {self.username}>'
